d0f47b88cd69ca667ec47f5445113abb52dd9aad
Luke Epps <lucasbepps@gmail.com>
2022-02-07 13:03:51 -0700

    CSS Updates

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function contactFunction() {
+    var box = document.getElementById("contact-box");
+    if (box.style.display === "none") {
+        box.style.display = "block";
+    } else {
+        box.style.display = "none";
+    }
+}
